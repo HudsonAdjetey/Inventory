4c3dd9b900c77517d0df9d397d97d7583ba6211f
HudsonAdjetey <adjetey545@gmail.com>
2024-08-14 14:59:41 -0700

    useSelectors event callers

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,20 +1,39 @@
+"use client";
 import React from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+import { useDispatch } from "react-redux";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const DashboardWrapper = ({ children }: Props) => {
+const DashboardLayout = ({ children }: Props) => {
+  const isSliderCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
   return (
     <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen light`}>
       <Sidebar />
-      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24 ">
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+          isSliderCollapsed ? "md:pl-24 " : "md:pl-72"
+        }`}
+      >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: Props) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
