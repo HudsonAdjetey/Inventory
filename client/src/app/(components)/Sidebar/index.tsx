a88920fe4de8d846dafeee837a130ad43161f9dd
HudsonAdjetey <adjetey545@gmail.com>
2024-08-14 15:23:16 -0700

    cpInfo

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,16 +1,31 @@
 "use client";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/app/state";
 import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSliderCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSliderCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSliderCollapsed ? "w-0 md:w-16" : "w-72 md:w-64 "
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-50`;
+
   return (
-    <div>
+    <div className={sidebarClassNames}>
       <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
         <div>logo</div>
         <h1 className="font-extrabold text-2xl">SwiftTech</h1>
         <button
           className="md:hidden px-3 py-3 bg-gray-100 rounded-full bover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
