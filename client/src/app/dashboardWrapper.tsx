e7e089a073160cb86b11499d64438c698ac3c09a
HudsonAdjetey <adjetey545@gmail.com>
2024-08-14 04:45:38 -0700

    sidebar added to wrapper component

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 type Props = {
   children: React.ReactNode;
@@ -8,7 +9,7 @@ type Props = {
 const DashboardWrapper = ({ children }: Props) => {
   return (
     <div className={`flex bg-gray-50 text-gray-900 w-full min-h-screen light`}>
-      sidebar
+      <Sidebar />
       <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24 ">
         <Navbar />
         {children}
